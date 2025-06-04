3901475f4f3c438d9ded922286870e70ba970b69
Justin White <jw@justinwhite.net>
2025-06-04 09:35:40 -0400

    implement load()

--- python/textsearcher.py
+++ python/textsearcher.py
@@ -1,13 +1,22 @@
 import string
 
+
 class TextSearcher(object):
+    file = None
 
     def __init__(self):
         pass
 
-    def load(self, file_path:str)->bool:
-        pass
-    
-    def search(self, word:str, context:int=0)->list:
-        pass
+    def load(self, file_path: str) -> bool:
+        if file_path is None:
+            return False
 
+        try:
+            self.file = open(file_path)
+        except FileNotFoundError:
+            return False
+
+        return True
+
+    def search(self, word: str, context: int = 0) -> list:
+        pass
