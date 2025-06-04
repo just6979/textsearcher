8447ef2bf76d4b735277e2c8f3c40f62a8646cfd
Justin White <jw@justinwhite.net>
2025-06-04 12:04:10 -0400

    one big context

--- python/textsearcher.py
+++ python/textsearcher.py
@@ -21,35 +21,38 @@ class TextSearcher(object):
     def search(self, word: str, context: int = 0) -> list:
         result = []
         lines = self.file.readlines()
-        cur_line = 0
+        words = []
         for line in lines:
-            line = line.strip()
-            words = line.split(' ')
-            if '' in words:
-                words.remove('')
-            if word in words:
-                clean_word = word.strip()
-                clean_word = clean_word.strip('"')
-                for x in range(line.count(clean_word)):
-                    if context > 0:
-                        loc = words.index(clean_word)
-                        context_start = loc - context
-                        context_end = loc + context + 1
-                        if context_start < 0:
-                            if cur_line > 0:
-                                words = lines[cur_line - 1].strip().split(' ')
-                                words.extend(lines[cur_line].strip().split(' '))
-                                print(words)
-                                loc = words.index(clean_word)
-                                context_start = loc - context
-                                context_end = loc + context + 1
-                            else:
-                                context_start = 0
-                        response = ' '.join(words[context_start: context_end])
-                        print(response)
-                        result.append(response)
-                    else:
-                        result.append(word)
-            cur_line += 1
+            temp_words = [word.strip() for word in line.strip().split()]
+            while '' in temp_words:
+                temp_words.remove('')
+            words.extend(temp_words)
+
+        print(words)
+
+        start_loc = 0
+        matches_remaining = True
+
+        while matches_remaining:
+            try:
+                match_loc = words.index(word, start_loc)
+            except ValueError:
+                break
+
+            start_loc = match_loc + 1
+
+            if context == 0:
+                result.append(word)
+                continue
+
+            context_start = match_loc - context
+            context_end = match_loc + context + 1
+            if context_start < 0:
+                context_start = 0
+            if context_end > len(words):
+                context_end = len(words)
+            response = ' '.join(words[context_start: context_end])
+            print(response)
+            result.append(response)
 
         return result
