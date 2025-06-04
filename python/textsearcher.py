642973d97b2d3c5afc830bfac4e6dccaef356ed1
Justin White <jw@justinwhite.net>
2025-06-04 11:24:44 -0400

    attempt at expanding context as needed

--- python/textsearcher.py
+++ python/textsearcher.py
@@ -20,15 +20,36 @@ class TextSearcher(object):
 
     def search(self, word: str, context: int = 0) -> list:
         result = []
-        for line in self.file:
+        lines = self.file.readlines()
+        cur_line = 0
+        for line in lines:
             line = line.strip()
             words = line.split(' ')
+            if '' in words:
+                words.remove('')
             if word in words:
-                for x in range(line.count(word)):
+                clean_word = word.strip()
+                clean_word = clean_word.strip('"')
+                for x in range(line.count(clean_word)):
                     if context > 0:
-                        loc = words.index(word)
-                        result.append(' '.join(words[loc - context: loc + context + 1]))
+                        loc = words.index(clean_word)
+                        context_start = loc - context
+                        context_end = loc + context + 1
+                        if context_start < 0:
+                            if cur_line > 0:
+                                words = lines[cur_line - 1].strip().split(' ')
+                                words.extend(lines[cur_line].strip().split(' '))
+                                print(words)
+                                loc = words.index(clean_word)
+                                context_start = loc - context
+                                context_end = loc + context + 1
+                            else:
+                                context_start = 0
+                        response = ' '.join(words[context_start: context_end])
+                        print(response)
+                        result.append(response)
                     else:
                         result.append(word)
+            cur_line += 1
 
         return result
