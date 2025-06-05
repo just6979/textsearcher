f6b9c18e500bf5378ad2120e50e78fe9967f2483
Justin White <jw@justinwhite.net>
2025-06-05 12:08:25 -0400

    passes all

---
+++ Hello.md
@@ -0,0 +1,1 @@
+It's pretty hacky, not very flexible, and uses way more memory than it needs to, but it passes all the tests.
--- python/textsearcher.py
+++ python/textsearcher.py
@@ -2,42 +2,39 @@ import string
 
 
 class TextSearcher(object):
-    file = None
 
     def __init__(self):
-        pass
+        self.shadow_words = []
+        self.words = []
 
     def load(self, file_path: str) -> bool:
         if file_path is None:
             return False
 
         try:
-            self.file = open(file_path)
+            file = open(file_path)
         except FileNotFoundError:
             return False
 
-        return True
-
-    def search(self, word: str, context: int = 0) -> list:
-        result = []
-        lines = self.file.readlines()
-        words = []
-        shadow_words = []
-
+        lines = file.readlines()
         for line in lines:
             # clean the line, get each word, clean the words, skip ''
             temp_words = [word.strip() for word in line.strip().split() if word != '']
             # make a shadow word list in all lowercase
             temp_shadow_words = [word.lower().strip().strip('"').strip('.') for word in temp_words]
             # save the line's words
-            words.extend(temp_words)
-            shadow_words.extend(temp_shadow_words)
+            self.words.extend(temp_words)
+            self.shadow_words.extend(temp_shadow_words)
 
+        return True
+
+    def search(self, word: str, context: int = 0) -> list:
+        result = []
         start_loc = 0
         while True:
             clean_word = word.strip().strip('"').strip('.').lower()
             try:
-                match_loc = shadow_words.index(clean_word, start_loc)
+                match_loc = self.shadow_words.index(clean_word, start_loc)
             except ValueError:
                 break
 
@@ -51,9 +48,9 @@ class TextSearcher(object):
             context_end = match_loc + context + 1
             if context_start < 0:
                 context_start = 0
-            if context_end > len(words):
-                context_end = len(words)
-            response = ' '.join(words[context_start: context_end])
+            if context_end > len(self.words):
+                context_end = len(self.words)
+            response = ' '.join(self.words[context_start: context_end])
             print(response)
             result.append(response)
 
